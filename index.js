6f4a08f819036ae833371b0b8630fc21164a4284
suryabo <surya.bonugu@ftr.com>
2024-06-03 16:38:31 -0600

    Node JS and Express Initialization

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+let app = express();
+app.set('port', process.env.PORT || 3005);
+
+app.get('/', function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    const response = { "response": "This is GET method." };
+    console.log(response);
+    res.end(JSON.stringify(response));
+});
+
+const server = app.listen(app.get("port"), function () {
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log("Node.js API app listening at http://%s:%s", host, port);
+});
